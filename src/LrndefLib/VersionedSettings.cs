5cefb9fae4155dca48fbb7680b97746a278cd636
Arthri <41360489+Arthri@users.noreply.github.com>
2021-10-11 14:55:09 +0800

    feat: Add base settings class

---
+++ src/LrndefLib/VersionedSettings.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace LrndefLib
+{
+    public abstract class VersionedSettings
+    {
+        [JsonProperty(
+            PropertyName = "version",
+            Order = int.MinValue,
+            Required = Required.Always)]
+        public Version SettingsVersion { get; }
+
+        [JsonIgnore]
+        public abstract Version CurrentVersion { get; }
+    }
+}
