d7950a683663859a2cdc72f2bbe8796bac7dda3d
Arthri <41360489+Arthri@users.noreply.github.com>
2021-10-15 19:07:47 +0800

    refactor: `SettingsMetadata` into another file

---
+++ src/LrndefLib/SettingsMetadata.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+
+namespace LrndefLib
+{
+    /// <summary>
+    /// Represents settings' metadata, such as what version it is.
+    /// </summary>
+    public sealed class SettingsMetadata
+    {
+        /// <summary>
+        /// Represents the metadata version it was written in.
+        /// </summary>
+        [JsonProperty("metadataVersion")]
+        public SimpleVersion MetadataVersion { get; }
+
+        /// <summary>
+        /// Represents the settings version it was written in.
+        /// </summary>
+        [JsonProperty("version")]
+        public SimpleVersion SettingsVersion { get; }
+
+        public SettingsMetadata(
+            SimpleVersion metadataVersion,
+            SimpleVersion settingsVersion)
+        {
+            MetadataVersion = metadataVersion;
+            SettingsVersion = settingsVersion;
+        }
+    }
+}
--- src/LrndefLib/VersionedSettings.cs
+++ src/LrndefLib/VersionedSettings.cs
@@ -8,37 +8,6 @@ namespace LrndefLib
     public abstract class VersionedSettings
     {
         /// <summary>
-        /// Represents settings' metadata, such as what version it is.
-        /// </summary>
-        public sealed class SettingsMetadata
-        {
-            /// <summary>
-            /// Represents the metadata version it was written in.
-            /// </summary>
-            [JsonProperty("metadataVersion")]
-            public SimpleVersion MetadataVersion { get; }
-
-            /// <summary>
-            /// Represents the JSON property name of <see cref="SettingsVersion"/>.
-            /// </summary>
-            public const string PROPNAME_SettingsVersion = "version";
-
-            /// <summary>
-            /// Represents the settings version it was written in.
-            /// </summary>
-            [JsonProperty(PROPNAME_SettingsVersion)]
-            public SimpleVersion SettingsVersion { get; }
-
-            public SettingsMetadata(
-                SimpleVersion metadataVersion,
-                SimpleVersion settingsVersion)
-            {
-                MetadataVersion = metadataVersion;
-                SettingsVersion = settingsVersion;
-            }
-        }
-
-        /// <summary>
         /// Represents the JSON property name of <see cref="Metadata"/>.
         /// </summary>
         public const string PROPNAME_Metadata = "metadata";
